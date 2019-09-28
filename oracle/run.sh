b9c7449ab6a43d7887a44f7b6900b67483275985
Hugh Wang <18501667737@163.com>
2019-09-28 15:08:01 +0800

    update

--- oracle/run.sh
+++ oracle/run.sh
@@ -1,1 +1,2 @@
 #!/usr/bin/env bash
+docker run -d --name orcl12g -p 1521:1521 -p 5500:5500 -v /mnt/data/ORCL:/ORCL store/oracle/database-enterprise:12.2.0.1
---
+++ redis/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -d --name redis_t1 -p 6379:6379 redis --requirepass Uwjx2019
